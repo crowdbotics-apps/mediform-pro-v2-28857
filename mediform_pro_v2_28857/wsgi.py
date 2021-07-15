3ef6de26d720929286f50e372bc5e91a5cb6f11a
Crowdbotics <team@crowdbotics.com>
2021-07-15 02:54:34 +0000

    Update models

--- mediform_pro_v2_28857/wsgi.py
+++ mediform_pro_v2_28857/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mediform_pro_v2_28857.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mediform_pro_v2_28857.settings")
 
 application = get_wsgi_application()
